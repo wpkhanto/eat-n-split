d5eb5e33eccb4dfd3e24d5d7bd89453946612882
Watcharaphon Khanto <wpkhanto@icloud.com>
2024-12-01 10:49:45 +0700

    Splitting a Bill

--- src/App.js
+++ src/App.js
@@ -41,6 +41,20 @@ export default function App() {
         setShowAddFriend(false);
     }
 
+    function handleSplitBill(value) {
+        console.log(value);
+
+        setFriends((friends) =>
+            friends.map((friend) =>
+                friend.id === selectedFriend.id
+                    ? { ...friend, balance: friend.balance + value }
+                    : friend
+            )
+        );
+
+        setSelectedFriend(null);
+    }
+
     return (
         <div className="app">
             <div className="sidebar">
@@ -58,7 +72,10 @@ export default function App() {
             </div>
 
             {selectedFriend && (
-                <FormSplitBill selectedFriend={selectedFriend} />
+                <FormSplitBill
+                    selectedFriend={selectedFriend}
+                    onSplitBill={handleSplitBill}
+                />
             )}
         </div>
     );
@@ -159,14 +176,22 @@ function FormAddFriend({ onAddFriend }) {
     );
 }
 
-function FormSplitBill({ selectedFriend }) {
+function FormSplitBill({ selectedFriend, onSplitBill }) {
     const [bill, setBill] = useState("");
     const [paidByUser, setPaidByUser] = useState("");
     const paidByFriend = bill ? bill - paidByUser : "";
     const [whoIsPaying, setWhoIsPaying] = useState("user");
 
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        if (!bill || !paidByUser) return;
+
+        onSplitBill(whoIsPaying === "user" ? paidByFriend : -paidByUser);
+    }
+
     return (
-        <form className="form-split-bill">
+        <form className="form-split-bill" onSubmit={handleSubmit}>
             <h2>Split a bill with {selectedFriend.name}</h2>
 
             <label>💰 Bill value</label>
